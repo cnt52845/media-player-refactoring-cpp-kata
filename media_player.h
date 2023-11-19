07afb510421e4d8eb1e2b931968ef1b536a85435
Replit user <>
2023-11-19 03:58:20 +0000

    Class work

--- media_player.h
+++ media_player.h
@@ -16,44 +16,113 @@ public:
     virtual void view_image()    = 0;
 };
 
-class AudioPlayer : public IMediaPlayer {
+class IAudioPlayer {
 public:
-    void play_audio() final
-    {
-        // Implementation...
-    }
-    void display_video() final { throw std::logic_error("Not implemented"); }
-    void view_image() final { throw std::logic_error("Not implemented"); }
+    virtual void play_audio(const MediaFile& file) = 0;
 };
 
-class VideoPlayer : public IMediaPlayer {
+class IVideoPlayer {
 public:
-    void play_audio() final { throw std::logic_error("Not implemented"); }
-    void display_video() final
-    {
-        // Implementation...
-    }
-    void view_image() final { throw std::logic_error("Not implemented"); }
+    virtual void display_video(const MediaFile& file) = 0;
 };
 
-class ImagePlayer : public IMediaPlayer {
+class IImageViewer {
 public:
-    void play_audio() final { throw std::logic_error("Not implemented"); }
-    void display_video() final { throw std::logic_error("Not implemented"); }
-    void view_image() final
+    virtual void view_image(const MediaFile& file) = 0;
+};
+
+class Mp3Player : public IAudioPlayer {
+public:
+    void play_audio(const MediaFile& file) final
     {
+        if (file.format != "mp3") {
+            throw std::invalid_argument("Invalid file format for Mp3Player!");
+        }
         // Implementation...
     }
 };
 
-using Players = std::vector<std::shared_ptr<IMediaPlayer>>;
+class FlacPlayer : public IAudioPlayer {
+public:
+    void play_audio(const MediaFile& file) final
+    {
+        if (file.format != "flac") {
+            throw std::invalid_argument("Invalid file format for FlacPlayer!");
+        }
+        // Implementation...
+    }
+};
+
+class WavPlayer : public IAudioPlayer {
+public:
+    void play_audio(const MediaFile& file) final
+    {
+        if (file.format != "wav") {
+            throw std::invalid_argument("Invalid file format for WavPlayer!");
+        }
+        // Implementation...
+    }
+};
+
+class JpgPlayer : public IImageViewer {
+public:
+    void view_image(const MediaFile& file) final
+    {
+        if (file.format != "jpg") {
+            throw std::invalid_argument("Invalid file format for JpgPlayer!");
+        }
+        // Implementation...
+    }
+};
+
+class PngPlayer : public IImageViewer {
+public:
+    void view_image(const MediaFile& file) final
+    {
+        if (file.format != "png") {
+            throw std::invalid_argument("Invalid file format for PngPlayer!");
+        }
+        // Implementation...
+    }
+};
+
+class Mp4Player : public IVideoPlayer {
+public:
+    void display_video(const MediaFile& file) final
+    {
+        if (file.format != "mp4") {
+            throw std::invalid_argument("Invalid file format for PngPlayer!");
+        }
+        // Implementation...
+    }
+};
+/*
+using AudioPlayers = std::vector<std::shared_ptr<IAudioPlayer>>;
+using VideoPlayers = std::vector<std::shared_ptr<IVideoPlayer>>;
+using ImagePlayers = std::vector<std::shared_ptr<IImagePlayer>>;
+*/
+struct Players {
+    std::map<std::string, std::shared_ptr<IAudioPlayer>> audio_players;
+    std::map<std::string, std::shared_ptr<IVideoPlayer>> video_players;
+    std::map<std::string, std::shared_ptr<IImageViewer>> image_players;
+};
 
 class MediaListPlayer {
 public:
     void play_list(const std::vector<MediaFile>& media_list, const Players& players)
     {
-        for (size_t i = 0; i < media_list.size(); i++) {
-            // Implementation...
+        for (const MediaFile& file : media_list) {
+            auto audio_player = players.audio_players.find(file.format);
+            if (audio_player != players.audio_players.end()) {
+                audio_player->second->play_audio(file);
+                continue;
+            }
+            auto video_player = players.video_players.find(file.format);
+            if (video_player != players.video_players.end()) {
+                video_player->second->display_video(file);
+                continue;
+            }
+            throw std::invalid_argument("Invalid file format!");
         }
     }
 };
--- test_media_player.cpp
+++ test_media_player.cpp
@@ -1,36 +1,119 @@
 #include <gmock/gmock.h>
+#include <stdexcept>
 
 #include "media_player.h"
 
-TEST(AudioPlayerTest, PlayAudio)
+TEST(AudioPlayerTest, Mp3PlayerHandlesMp3)
 {
-    AudioPlayer audio_player;
-    EXPECT_NO_THROW(audio_player.play_audio());
-    EXPECT_THROW(audio_player.display_video(), std::logic_error);
-    EXPECT_THROW(audio_player.view_image(), std::logic_error);
+    Mp3Player mp3_player;
+    MediaFile mp3_file;
+    mp3_file.format = "mp3";
+    EXPECT_NO_THROW(mp3_player.play_audio(mp3_file));
 }
 
-TEST(VideoPlayerTest, DisplayVideo)
+TEST(AudioPlayerTest, Mp3PlayerRejectsNonMp3)
 {
-    VideoPlayer videoPlayer;
-    EXPECT_NO_THROW(videoPlayer.display_video());
-    EXPECT_THROW(videoPlayer.play_audio(), std::logic_error);
-    EXPECT_THROW(videoPlayer.view_image(), std::logic_error);
+    Mp3Player mp3_player;
+    MediaFile flac_file;
+    flac_file.format = "flac";
+    EXPECT_THROW(mp3_player.play_audio(flac_file), std::invalid_argument);
 }
 
-TEST(ImagePlayerTest, ViewImage)
+TEST(AudioPlayerTest, FlacPlayerHandlesFlac)
 {
-    ImagePlayer imagePlayer;
-    EXPECT_NO_THROW(imagePlayer.view_image());
-    EXPECT_THROW(imagePlayer.play_audio(), std::logic_error);
-    EXPECT_THROW(imagePlayer.display_video(), std::logic_error);
+    FlacPlayer flac_player;
+    MediaFile  flac_file;
+    flac_file.format = "flac";
+    EXPECT_NO_THROW(flac_player.play_audio(flac_file));
+}
+
+TEST(AudioPlayerTest, FlacPlayerRejectsNonFlac)
+{
+    FlacPlayer flac_player;
+    MediaFile  mp3_file;
+    mp3_file.format = "mp3";
+    EXPECT_THROW(flac_player.play_audio(mp3_file), std::invalid_argument);
+}
+
+TEST(AudioPlayerTest, WavPlayerHandlesWav)
+{
+    WavPlayer wav_player;
+    MediaFile wav_file;
+    wav_file.format = "wav";
+    EXPECT_NO_THROW(wav_player.play_audio(wav_file));
+}
+
+TEST(AudioPlayerTest, WavPlayerRejectsNonWav)
+{
+    WavPlayer wav_player;
+    MediaFile mp3_file;
+    mp3_file.format = "mp3";
+    EXPECT_THROW(wav_player.play_audio(mp3_file), std::invalid_argument);
+}
+
+TEST(ImagePlayerTest, JpgPlayerHandlesJpg)
+{
+    JpgPlayer jpg_player;
+    MediaFile media_file;
+    media_file.format = "jpg";
+    EXPECT_NO_THROW(jpg_player.view_image(media_file));
+}
+
+TEST(ImagePlayerTest, JpgPlayerRejectsNonJpg)
+{
+    JpgPlayer jpg_player;
+    MediaFile media_file;
+    media_file.format = "png";
+    EXPECT_THROW(jpg_player.view_image(media_file), std::invalid_argument);
+}
+
+TEST(ImagePlayerTest, PngPlayerHandlesJPng)
+{
+    PngPlayer png_player;
+    MediaFile media_file;
+    media_file.format = "png";
+    EXPECT_NO_THROW(png_player.view_image(media_file));
+}
+
+TEST(ImagePlayerTest, PngPlayerRejectsNonPng)
+{
+    PngPlayer png_player;
+    MediaFile media_file;
+    media_file.format = "jpg";
+    EXPECT_THROW(png_player.view_image(media_file), std::invalid_argument);
+}
+
+TEST(VideoPlayerTest, Mp4PlayerHandlesMp4)
+{
+    Mp4Player mp4_player;
+    MediaFile media_file;
+    media_file.format = "mp4";
+    EXPECT_NO_THROW(mp4_player.display_video(media_file));
+}
+
+TEST(VideoPlayerTest, Mp4PlayerRejectsNonMp4)
+{
+    Mp4Player mp4_player;
+    MediaFile media_file;
+    media_file.format = "mkv";
+    EXPECT_THROW(mp4_player.display_video(media_file), std::invalid_argument);
 }
 
 TEST(MediaListPlayerTest, MediaListPlayerPlaysCompatibleList)
 {
-    std::vector<MediaFile> media_list{/* populate media files */};
-    Players                players{/* populate players */};
+    std::vector<MediaFile> media_list{
+        {"mp3", "file.mp3"}, {"wav", "file.wav"}, {"mp4", "file.mp4"}};
+    Players players{.audio_players{
+                        {"mp3", std::make_shared<Mp3Player>()},
+                        {"wav", std::make_shared<WavPlayer>()},
+                    },
+
+                    .video_players{{"mp4", std::make_shared<Mp4Player>()}}};
+
+    // AudioPlayers players{new Mp3Player(), new FlacPlayer(), new WavPlayer()};
 
     MediaListPlayer list_player;
+    // AudioListPlayer list_player;
+
     EXPECT_NO_THROW(list_player.play_list(media_list, players));
 }
